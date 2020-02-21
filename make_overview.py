d67e8bacc3b0e417ca757d5a4c81eb78ca8e0583
Alexander Mann <mann@cern.ch>
2020-02-21 11:25:18 +0100

    add tables to make_overview.py

--- make_overview.py
+++ make_overview.py
@@ -5,6 +5,9 @@
 # 
 # Run with:
 #   python make_overview.py && pdflatex -interaction=batchmode overview.tex
+# Or with latex-make:
+#   python make_overview.py && latexmk --pdf overview.tex
+# 
 #   
 # Produces 
 #   overview.pdf
@@ -20,6 +23,7 @@ from __future__ import print_function # for compatibility with Python 2
 import os
 import glob
 import json
+import pandas as pd
 
 
 ### helpers
@@ -30,25 +34,84 @@ def StringToLatex(s):
   s = s.replace("~", "\~")
   return s
 
+
+def LoadXsecJSONAsDF(jsondata):
+  
+  # convert to pandas.DataFrame
+  df   = pd.DataFrame.from_dict(jsondata["data"], orient = "index")
+  
+  # restore mass as column and sort 
+  df["mass_GeV"] = df.index.astype(int)
+  df = df.sort_values("mass_GeV")
+  df.reset_index(inplace = True, drop = True)
+  
+  # compute fields with percentage uncertainties
+  for v in ["", "down_", "up_"]:
+    if ('unc_%spb' % v) in df.columns:
+      df['unc_%s%%' % v] = df['unc_%spb' % v] / df['xsec_pb'] * 100
+  
+  return df
+
   
 ### main
 path_out = "overview.tex"
 with open(path_out, "w") as outf:
   
-  print(r"""\documentclass[a4paper,10pt]{scrartcl} 
-  \usepackage[utf8]{inputenc}
-  \begin{document}""", file=outf)
+  # header
+  print(r"""\documentclass[a4paper,10pt]{scrartcl}
+    \usepackage[utf8]{inputenc}
+    \usepackage{booktabs}
+    \usepackage{longtable}
+    \usepackage{hyperref}
+    \hypersetup{
+      colorlinks=true,
+      linkcolor=blue
+    }
+    \begin{document}
+    \section{Descriptions}
+    """, file=outf)
 
   filenames = sorted(glob.glob("json/*.json"))
   print("Found %d JSON files in repository:\n\\begin{itemize}" % len(filenames), file=outf)
-  for filename in filenames:
+  
+  # overview
+  for idx, filename in enumerate(filenames):
     data = json.load(open(filename))
     print(r"\item %s (\texttt{%s})" % (data["process_latex"], StringToLatex(filename)), file=outf)
     print(r"\begin{itemize}", file=outf) 
     print(r"\item %s" % StringToLatex(data["comment"]), file=outf)
+    print(r"\item see section \ref{tab:xsec%d}" % idx, file=outf)
     print(r"\end{itemize}", file=outf)
     
-  print(r"\end{itemize}\end{document}", file=outf)
+  print(r"\end{itemize}\section{Tables}", file = outf)
+  
+  # tables
+  for idx, filename in enumerate(filenames):
+    data = json.load(open(filename))
+    print(r"\subsection{Cross sections for %s}\label{tab:xsec%d}" % (data["process_latex"], idx), file = outf)
+    try:
+      df = LoadXsecJSONAsDF(data)
+    except Exception as e:
+      print("Failed to convert data from %s (%s)" % (filename, str(e)))
+      print(r"(Failed to read data.)", file = outf)
+      continue
+    
+    print(r"""\begin{longtable}{ccc}
+      \toprule 
+      Mass [GeV] & cross section [pb] & uncertainty [\%] \\
+      \midrule\endhead""", file = outf)
+    for idx, row in df.iterrows():
+      uncs = "[unknown]"
+      if row.get("unc_%") is not None:
+        uncs = "$\pm$%.1f" % row.get("unc_%")
+      elif row.get("unc_down_%") is not None:
+        uncs = "-%.1f / +%.1f" % (row.get("unc_down_%"), row.get("unc_up_%"))
+      print(r"%s & %g & %s \\" % (row.mass_GeV, float(row.xsec_pb), uncs), file=outf)
+    print(r"\bottomrule\end{longtable}", file = outf)
+  
+  
+  # footer
+  print(r"\end{document}", file=outf)
             
-print("Wrote %s" % path_out)
+print("Wrote LaTeX output to %s" % path_out)
 
